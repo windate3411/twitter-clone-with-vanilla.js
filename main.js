8aebbd1ce69c94a0100ec2d8ee3dee320411eb3e
windate3411 <windate3411@gmail.com>
2020-09-06 19:47:57 +0800

    add display pages functions

---
+++ main.js
@@ -0,0 +1,34 @@
+// selectDOM elements
+
+const mainPage = document.querySelector('.main-page')
+const loginPage = document.querySelector('.login-page')
+const middleContent = document.querySelector('.middle-content')
+const btnTop = document.querySelector('.btn-top')
+const newFeedsPage = document.querySelector('.feeds-page')
+
+// handlers
+
+const goToLoginPage = () => {
+  changeElementDisplay(mainPage, 'none')
+  changeElementDisplay(loginPage, 'grid')
+}
+
+middleContent.addEventListener('click', (e) => {
+  if (e.target.classList[1] === 'main-btn') goToLoginPage()
+})
+
+btnTop.addEventListener('click', (e) => {
+  const inputUserInfo = document.querySelector('.user-info')
+  const inputUserPassword = document.querySelector('.user-password')
+
+  if (inputUserInfo.value && inputUserPassword) {
+    changeElementDisplay(newFeedsPage, 'block')
+    changeElementDisplay(mainPage, 'none')
+    return
+  }
+  goToLoginPage()
+})
+
+function changeElementDisplay(element, newDisplay) {
+  element.style.display = newDisplay
+}
